8e0306590f351dd8b586f83d622c228e32cdd4a3
Jumpei Arima <arijun0307@gmail.com>
2019-11-21 19:08:53 +0900

    add sub twist msgs

--- py_roomba_adapter_ros2/roomba_node.py
+++ py_roomba_adapter_ros2/roomba_node.py
@@ -7,6 +7,7 @@ import rclpy
 from rclpy.node import Node
 from rclpy.parameter import Parameter
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
 from pyroombaadapter import PyRoombaAdapter
 
@@ -14,9 +15,16 @@ class RoombaNode(Node):
     def __init__(self):
         print('Roomba node!')
         super().__init__('roomba_node')
+        
+        PORT = "/dev/ttyUSB0"
+        self.adapter = PyRoombaAdapter(PORT)
+        self.twist_sub = self.create_subscription(Twist, "/cmd_vel", self.twist_callback)
 
     def twist_callback(self, msg):
         print('callback')
+        velocity = msg.linear.x
+        yaw_rate = msg.angular.z
+        self.adapter.move(velocity, yaw_rate)
 
 def main(args=None):
     rclpy.init(args=args)
